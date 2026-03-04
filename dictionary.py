1fa16666aad61effef207620101b44de61a3a88c
RavshanovDilmurod <169117371+RavshanovDilmurod@users.noreply.github.com>
2026-03-04 14:11:24 +0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,39 +1,98 @@
-""" 
+# # """ 
 
-Sana 03:03:26 yil 
+# # Sana 03:03:26 yil 
 
-Ustoz : Anvar Narzullayev 
+# # Ustoz : Anvar Narzullayev 
 
-Mavzu: Dictionary ( pythonda lug'at tushunchasi )
+# # Mavzu: Dictionary ( pythonda lug'at tushunchasi )
 
- """
+# #  """
  
-# en_uz = {'hello':'salom', 'bye':'xayr', 'apple':'olma',}
+# # # en_uz = {'hello':'salom', 'bye':'xayr', 'apple':'olma',}
 
-# # print(en_uz['hello'])
+# # # # print(en_uz['hello'])
 
-# fruits = {'olma':'15000', 'uzum':'10000', 'nok':'12000'}
+# # # fruits = {'olma':'15000', 'uzum':'10000', 'nok':'12000'}
 
-# teacher = {'name':'Anvar Narzullayev', 'yosh':'50', 't_yil':'1976'}
+# # # teacher = {'name':'Anvar Narzullayev', 'yosh':'50', 't_yil':'1976'}
 
-# print(teacher['name'].title(), teacher['yosh'], "yoshda va", teacher['t_yil'], "da tug'ilgan")
+# # # print(teacher['name'].title(), teacher['yosh'], "yoshda va", teacher['t_yil'], "da tug'ilgan")
 
-user_data = []
+# # user_data = []
 
-name = input("Ismingizni kiriting:_")
+# # name = input("Ismingizni kiriting:_")
 
-name = name.title()
+# # name = name.title()
 
-age = input("Yoshingizni kiriting:_")
+# # age = input("Yoshingizni kiriting:_")
 
-birth = input("Tug'ilgan yilingizni kiriting:_")
+# # birth = input("Tug'ilgan yilingizni kiriting:_")
 
-user_data.append(name)
+# # user_data.append(name)
 
-user_data.append(age)
+# # user_data.append(age)
 
-user_data.append(birth)
+# # user_data.append(birth)
 
-# print(user_data)
+# # user_request_name = {}
 
-print("Ma'lumotlaringizdan birini so'rang: ism, yosh, t_yil tarzda yozing !")
+# # user_request_name = user_data(name)
+
+# # user_request_name = input("Ismingizni so'rang: 'ism' tarzda yozing !")
+
+# # user_request_name = user_data.append(name) 
+
+
+# # # print(user_data)
+
+
+# # print("Ismingiz: ", user_request_name)
+
+# name = input(("Ismingizni kiriting:_"))
+
+# age = input(("Yoshingizni kiriting:_"))
+
+# birth = input(("Tug'ilgan yilingizni kiriting:_"))
+
+# user_data = { 'name':name, 'age':age, 'birth':birth }    
+
+# kalit = input("Ma'lumotlaringizdan birini yozing: (name,age,birth) deb yozing:_")
+
+# if kalit == 'name':
+    
+#     print("Ismingiz:_", user_data[kalit].title())
+    
+# elif kalit == 'age':
+    
+#     print("Yoshingiz:_", user_data[kalit])
+    
+# elif kalit == 'birth':
+    
+#     print("Tug'ilgan yilingiz:_", user_data[kalit])    
+
+# else:
+    
+#      print("Noto'g'ri kalit so'z kiritdingiz:")
+     
+# # print("Ismingiz:_", name.title())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
