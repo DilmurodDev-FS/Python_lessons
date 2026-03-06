d3e7c53bc6f68ed9c16497f77681ced67b647537
RavshanovDilmurod <169117371+RavshanovDilmurod@users.noreply.github.com>
2026-03-06 14:36:45 +0500

    added input_while.py file
    while loop lesson

---
+++ input_while.py
@@ -0,0 +1,118 @@
+"""
+Sana 06:03:26 yil 01:01pm
+
+Ustoz: Anvar Narzullayev 
+
+Mazvu: Input va While sikli
+
+"""
+# son = 1
+
+# while son <= 10:
+         
+#     print(son**2,"\n")
+   
+#     son+=1
+
+# Foydalanuvchi kiritgan sonni kvadratini chiqaradigan dastur
+
+# print("Kiritgan soningizni kvadratini chiqarib beradigan dasturga xush kelibsiz")
+
+# savol = "Istalgan son kiriting:"
+
+# savol += " ( Dasturni to'xtatish uchun exit deb yozing ) " 
+
+# i = ""
+
+# while i !="exit":
+    
+#     i = input(savol)
+
+#     if i !="exit":
+
+#        print(float(i)**2)
+        
+# print("Dastur tugadi.")        
+  
+# Foydalanuvchi kiritgan sonni kvadratini chiqaradigan dastur
+
+# print("Kiritgan soningizni kvadratini chiqarib beradigan dasturga xush kelibsiz")
+
+# savol = "Istalgan son kiriting:"
+
+# savol += "( Dasturni to'xtatish uchun exit deb yozing )" 
+
+# i = True
+
+# while i:
+    
+#     i = input(savol)
+
+#     if i =="exit":
+        
+#        i = False
+       
+#     else:
+
+#        print(float(i)**2)
+        
+# print("Dastur tugadi.")      
+
+         # break-Bu siklni to'xtatadigan buyruq
+
+# print("Kiritgan soningizni kvadratini chiqarib beradigan dasturga xush kelibsiz")
+
+# savol = "Istalgan son kiriting:"
+
+# savol += "( Dasturni to'xtatish uchun exit deb yozing )" 
+
+# i = ""
+
+# while True:
+    
+#     i = input(savol)
+    
+#     if i =="exit":
+    
+#         break
+    
+#     else:
+        
+#         print(float(i)**2)
+        
+# print("Dastur tugadi.")     
+
+        # for siklida breakni qo'llash
+        
+# num = list(range(0,11))
+
+# for i in num:
+    
+#     if i == 5:
+        
+#         break
+    
+#     else:
+    
+#         print(i, "sonining kvadrati ", i**2)
+        
+        # continue - bu siklda 1ta qadam tashlab ketish operatori        
+        
+# num = list(range(0,11))
+
+# for i in num:
+     
+#      if i == 5:
+         
+#          continue
+          
+#      else:
+     
+#          print(i, "sonining kvadrati ", i**2)       
+        
+        
+        
+        
+        
+        
+        
