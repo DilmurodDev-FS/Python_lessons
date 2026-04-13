fac78c7c673dcd7e10136de6027fa4a7c62ff4ce
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-13 12:28:29 +0500

    updated

--- Birinchi_dastur_print.py
+++ Birinchi_dastur_print.py
@@ -2,18 +2,6 @@ print ("Assalomu Aleykum")
 
 print("Fs Repo")
 
-print("Updated all files")
-
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
-print("Updated all files")
 
 
 
