eafe22cad60bccdcde85128c29625616622e1f58
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-20 08:15:38 +0500

    Update funksiyaga_ro'yhat_uzatish.py

--- funksiyaga_ro'yhat_uzatish.py
+++ funksiyaga_ro'yhat_uzatish.py
@@ -7,16 +7,27 @@ Ustoz: Anvar Narzullayev
 Mavzu: funksiyaga ro'yhat yuborish
 
 """
+#
+# def bahola(ismlar):
+#     baholar = {}
+#     while ismlar:
+#         ism = ismlar.pop()
+#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+#         baholar[ism] = baho
+#     return baholar
+#
+# talabalar = ['dilmurod', 'otabek', 'sardor', 'xolbekjon']
+#
+# baholar = bahola(talabalar)
+# print(baholar)
 
-def bahola(ismlar):
-    baholar = {}
-    while ismlar:
-        ism = ismlar.pop()
-        baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
-        baholar[ism] = baho
-    return baholar
+""" Sana 20:04:2026 yil
+Ustoz: Anvar Narzullayev """
 
-talabalar = ['dilmurod', 'otabek', 'sardor', 'xolbekjon']
+def funksiyaga(ruyxat_nomi):
 
-baholar = bahola(talabalar)
-print(baholar)
+    print(ruyxat_nomi)
+
+sonlar = [1,2,3,4,5]
+
+funksiyaga(sonlar)
