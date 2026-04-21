f48f07c97c0909e11e3c616e9d13fc8f8466908f
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-21 21:45:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-kvadrat = lambda x, y: x**y
+# kvadrat = lambda x, y: x**y
+#
+# print(kvadrat(3,2))
 
-print(kvadrat(3,2))
+mevalar =['olma', 'anor', 'behi', 'banan', 'qovun']
+
+harf ='o'
+
+# meva_h = list(filter(lambda x:x.startswith(harf), mevalar))
+#
+# print(meva_h)
+
+meva_len = list(filter(lambda x:(len(x) <= 4 and x.startswith(harf),), mevalar))
+
+print(meva_len)
